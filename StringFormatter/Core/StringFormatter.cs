f9b0a6b4eb99e0eb91f880307a4d015d6a454023
Pcheloslav <slav.malink@gmail.com>
2022-12-11 14:24:02 +0300

    project structure

---
+++ StringFormatter/Core/StringFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class StringFormatter : IStringFormatter
+    {
+        public static readonly StringFormatter Shared = new StringFormatter();
+
+        public string Format(string template, object target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
